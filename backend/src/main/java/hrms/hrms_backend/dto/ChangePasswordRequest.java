d5fafe6964d321f4034da5ee06cdd3a3a9f5e2c6
SaiDeepakPokuri <saideepak.pokuri@gmail.com>
2026-03-27 12:13:58 +0530

    change password dto

---
+++ backend/src/main/java/hrms/hrms_backend/dto/ChangePasswordRequest.java
@@ -0,0 +1,10 @@
+package hrms.hrms_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+    private String email;
+    private String oldPassword;
+    private String newPassword;
+}
