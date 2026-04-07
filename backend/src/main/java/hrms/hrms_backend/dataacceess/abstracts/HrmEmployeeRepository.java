e4550685207ac114184cad9d4ddf0ffa4a8f343d
SaiDeepakPokuri <saideepak.pokuri@gmail.com>
2026-04-07 14:51:17 +0530

    added a new method

--- backend/src/main/java/hrms/hrms_backend/dataacceess/abstracts/HrmEmployeeRepository.java
+++ backend/src/main/java/hrms/hrms_backend/dataacceess/abstracts/HrmEmployeeRepository.java
@@ -1,8 +1,10 @@
 package hrms.hrms_backend.dataacceess.abstracts;
 
 import hrms.hrms_backend.entities.concretes.HrmEmployee;
+import hrms.hrms_backend.entities.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HrmEmployeeRepository extends JpaRepository<HrmEmployee, Long> {
@@ -10,4 +12,6 @@ public interface HrmEmployeeRepository extends JpaRepository<HrmEmployee, Long> 
     boolean existsByEmail(String email);
 
     Optional<HrmEmployee> findByEmail(String email);
+
+    List<HrmEmployee> findByRoleIn(List<Role> roles);
 }
