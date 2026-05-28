855bae7ba4dea4b2bf58dfab462d2e42dffeb69f
Sai Deepak <saideepak.pokuri@gmail.com>
2026-05-28 13:02:02 +0530

    Added protected routing for WorkLog module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,72 +10,82 @@ import Profile from "./pages/Profie";
 import Login from "./pages/Login";
 import ChangePassword from "./pages/ChangePassword";
 import { Toaster } from "react-hot-toast";
+import WorkLog from "./pages/WorkLog";
 
 function App() {
   return (
     <>
-    <Toaster position="top-right" />
-    <BrowserRouter>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/change-password" element={<ChangePassword />} />
+      <Toaster position="top-right" />
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/change-password" element={<ChangePassword />} />
 
-        {/* Protected routes */}
-        <Route 
-          element={
-            <ProtectedRoute>
-              <MainLayout />
-            </ProtectedRoute>
-          }
-        >
+          {/* Protected routes */}
           <Route
-            path="/"
             element={
-              <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN"]}>
-                <Employees />
+              <ProtectedRoute>
+                <MainLayout />
               </ProtectedRoute>
             }
-          />
+          >
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN"]}>
+                  <Employees />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/attendance"
-            element={
-              <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN", "MANAGER", "EMPLOYEE"]}>
-                <Attendance />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/attendance"
+              element={
+                <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN", "MANAGER", "EMPLOYEE"]}>
+                  <Attendance />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/leaves"
-            element={
-              <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN", "MANAGER", "EMPLOYEE"]}>
-                <Leaves />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/reports"
-            element={
-              <ProtectedRoute
-                allowedRoles={[ROLES.HR_ADMIN, ROLES.SUPER_ADMIN]}
-              >
-                <Reports />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute allowedRoles={[ROLES.EMPLOYEE]}>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            <Route
+              path="/worklogs"
+              element={
+                <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN", "MANAGER", "EMPLOYEE"]}>
+                  <WorkLog />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/leaves"
+              element={
+                <ProtectedRoute allowedRoles={["SUPER_ADMIN", "HR_ADMIN", "MANAGER", "EMPLOYEE"]}>
+                  <Leaves />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/reports"
+              element={
+                <ProtectedRoute
+                  allowedRoles={[ROLES.HR_ADMIN, ROLES.SUPER_ADMIN]}
+                >
+                  <Reports />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute allowedRoles={[ROLES.EMPLOYEE]}>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
