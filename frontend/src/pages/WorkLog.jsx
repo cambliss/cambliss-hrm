e020cf788ae555ef63b63bec98ae33b5e4d5292e
SaiDeepakPokuri <saideepak.pokuri@gmail.com>
2026-06-19 12:10:58 +0530

    normalised import formatting

--- frontend/src/pages/WorkLog.jsx
+++ frontend/src/pages/WorkLog.jsx
@@ -1,21 +1,11 @@
-import EmployeeWorkLogDashboard
-    from "../components/EmployeeWorkLogDashboard";
-
-import WorkLogManagement
-    from "../components/WorkLogManagement";
-
-import { useAuth }
-    from "../context/AuthContext";
-
-import { ROLES }
-    from "../config/roles";
+import EmployeeWorkLogDashboard from "../components/EmployeeWorkLogDashboard";
+import WorkLogManagement from "../components/WorkLogManagement";
+import { useAuth } from "../context/AuthContext";
+import { ROLES } from "../config/roles";
 
 const WorkLog = () => {
-
     const { user } = useAuth();
-
-    const isEmployee =
-        user.role === ROLES.EMPLOYEE;
+    const isEmployee = user.role === ROLES.EMPLOYEE;
 
     return isEmployee
         ? <EmployeeWorkLogDashboard />
