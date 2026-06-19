c2b1002f49d497361f5c89dab1cf8ee62606a14d
SaiDeepakPokuri <saideepak.pokuri@gmail.com>
2026-06-19 12:15:32 +0530

    extracted shared input class strings into formStyles config

---
+++ frontend/src/config/formStyles.js
@@ -0,0 +1,15 @@
+export const inputCls =
+    "w-full px-3 py-2 rounded-md text-xs text-[#333333] bg-white " +
+    "border border-[#EBEBEB] placeholder-[#AAAAAA] " +
+    "focus:outline-none focus:border-[#C9A227] focus:ring-1 focus:ring-[#C9A227]/30 " +
+    "transition-colors duration-150";
+
+export const inputDisabledCls =
+    "w-full px-3 py-2 rounded-md text-xs text-[#AAAAAA] " +
+    "bg-[#F8F5EE] border border-[#EBEBEB] cursor-not-allowed";
+
+export const textareaCls =
+    "w-full px-3 py-2 rounded-md text-xs text-[#333333] bg-white resize-none " +
+    "border border-[#EBEBEB] placeholder-[#AAAAAA] " +
+    "focus:outline-none focus:border-[#C9A227] focus:ring-1 focus:ring-[#C9A227]/30 " +
+    "transition-colors duration-150";
